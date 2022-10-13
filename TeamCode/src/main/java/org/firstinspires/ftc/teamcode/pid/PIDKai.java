c1236e0f4c9c79f24d36281b057c558a34f2adc3
Kai Wallis <kona4kai@gmail.com>
2022-10-13 14:38:24 -0700

    Working PID with P and PI options

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pid/PIDKai.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pid/PIDKai.java
@@ -10,12 +10,36 @@ public class PIDKai
     public double lastE; //last error value
     public double totalE; //integral of errors
 
-    //Constructor Method
+    //Constructor Methods
     public PIDKai(double porportionalConstant, double integralConstant, double derivativeConstant)
     {
         kP = porportionalConstant;
         kI = integralConstant;
         kD = derivativeConstant;
+    }
+
+    //P function
+    public PIDKai(double porportionalConstant)
+    {
+        kP = porportionalConstant;
+        kI = 0;
+        kD = 0;
+    }
+
+    //PI function
+    public PIDKai(double porportionalConstant, double integralConstant)
+    {
+        kP = porportionalConstant;
+        kI = integralConstant;
+        kD = 0;
+    }
+
+    //sets function to not affect output
+    public PIDKai()
+    {
+        kP = 0;
+        kI = 0;
+        kD = 0;
     }
     
     //returns a control signal based on input expected and the actual value.
@@ -31,11 +55,11 @@ public class PIDKai
         return u;
     }
 
-    //adds derivative between error and lastE to total derivative
+    //adds to total integral based on current and past error
     //private to safegaurd accidental additions to totalE
     private double getI(double error)
     {
-        totalE += 0.5*(lastE + error); //0.5*(lastE-error) + error simplified
+        totalE = 0.66*totalE + 0.5*(lastE + error); //0.5*(lastE-error) + error simplified + past value of E (devalued)
         return totalE;
     }
 
