db5fc4678a622164ec3f8704c4be92e75826fb06
Kai Wallis <kona4kai@gmail.com>
2022-10-13 00:09:20 -0700

    Example runner of PIDKai, untuned constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pid/PIDTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pid/PIDTester.java
@@ -7,14 +7,19 @@ public class PIDTester
         //creates PIDKai object with arbitrary values to test
         PIDKai pid = new PIDKai(0.5, 0.2, 1);
 
+        //accesses constants
+        System.out.println("\nPorportional constant: " + pid.kP);
+        System.out.println("Integral constant: " + pid.kI);
+        System.out.println("Derivative constant: " + pid.kD);
+
         //runs methods to test
         System.out.println("lastE: " + pid.lastE);
+        System.out.println("totalE: " + pid.totalE);
         System.out.println("Out: " + pid.getPID(200, 100));
         System.out.println("lastE: " + pid.lastE);
+        System.out.println("totalE: " + pid.totalE);
         System.out.println("Out: " + pid.getPID(-20, 30));
         System.out.println("lastE: " + pid.lastE);
-
-        System.out.println("should equal kP*value: " + pid.getP(0.5));
-        System.out.println("Porportional constant: " + pid.kP);
+        System.out.println("totalE: " + pid.totalE);
     }
 }
