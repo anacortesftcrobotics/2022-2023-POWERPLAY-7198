d93533eff837bb044a56f2b767b3ec2e8b0bda9c
Kai G <grignkai000@stu.asd103.org>
2022-11-21 16:44:16 -0800

    My first attempts at a class to take care of sensor things
    Signed-off-by: Coach <22876098+anacortesftcrobotics@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/SensorSystem.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.chKai;
+
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+public class SensorSystem {
+    private ColorSensor color;
+    private DistanceSensor distance;
+
+    public void setup(HardwareMap hardwareMap, String sensorName){
+        color = hardwareMap.get(ColorSensor.class, "color");
+        distance = hardwareMap.get(DistanceSensor.class, "distance");
+
+    }
+    public int identify (){ //x=s 1 if red, 2 if green, 3 if blue, and 0 if nothing.
+        int x=0;
+        if(color.red() < 40 && color.green()< 60 && color.blue() < 45){
+            x= 0;
+        }else{
+            if (color.red()>color.blue()&&color.red()> color.green()){
+                x= 1;
+            } else if (color.green()>color.blue()&&color.green()> color.red()){
+                x= 2;
+            }else if (color.blue()>color.red()&&color.blue()> color.green()){
+                x= 1;
+            }
+        }
+        return x;
+    }
+    public String colorTelemetry(){
+        int red = color.red();
+        int green = color.green();
+        int blue = color.blue();
+        return ("red - "+ red+"\ngreen - "+green+"\nblue - "+blue);
+    }
+}
+
