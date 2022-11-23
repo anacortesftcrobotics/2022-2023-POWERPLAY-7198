e19d1d60c8cecb7ac0e0ebf6393e72c1473294f3
Kai G <grignkai000@stu.asd103.org>
2022-11-22 16:24:46 -0800

    Further attempts at a class to take care of sensor things
    Signed-off-by: Coach <22876098+anacortesftcrobotics@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/SensorSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/SensorSystem.java
@@ -14,9 +14,10 @@ public class SensorSystem {
         distance = hardwareMap.get(DistanceSensor.class, "distance");
 
     }
+
     public int identify (){ //x=s 1 if red, 2 if green, 3 if blue, and 0 if nothing.
         int x=0;
-        if(color.red() < 40 && color.green()< 60 && color.blue() < 45){
+        if(color.red() < 150 && color.green()< 150 && color.blue() < 150){
             x= 0;
         }else{
             if (color.red()>color.blue()&&color.red()> color.green()){
@@ -29,11 +30,27 @@ public class SensorSystem {
         }
         return x;
     }
+    public int identify2(){//returns 1 if cyan, 2 if yellow, 3 if magenta, and 0 if nothing.
+        int x=0;
+        if(color.red() < 150 && color.green()< 150 && color.blue() < 150){
+            x= 0;
+        }else{
+            if (color.blue()>color.red()&&color.green()> color.red()){
+                x= 1;
+            } else if (color.red()>color.blue()&&color.green()> color.blue()){
+                x= 2;
+            }else if (color.red()>color.green()&&color.blue()> color.green()){
+                x= 1;
+            }
+        }
+        return x;
+    }
     public String colorTelemetry(){
         int red = color.red();
         int green = color.green();
         int blue = color.blue();
         return ("red - "+ red+"\ngreen - "+green+"\nblue - "+blue);
     }
+
 }
 
