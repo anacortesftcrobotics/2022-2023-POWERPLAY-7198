fd65ab05a467de98e7f9f528fe5003206bb534cb
KaiG1024 <118638427+KaiG1024@users.noreply.github.com>
2022-12-24 20:21:08 -0800

    starting the speed class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/Speed.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.chKai;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.powerplay.*;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.*;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+public class Speed {
+    
+}
