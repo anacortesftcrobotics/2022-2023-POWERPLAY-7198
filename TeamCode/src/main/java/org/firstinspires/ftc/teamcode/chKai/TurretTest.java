bacae5808bb67cc9f89118ea93e965b666b43b07
Kai G <grignkai000@stu.asd103.org>
2023-02-17 17:27:47 -0800

    TurretTest may or may not be working
    Signed-off-by: Coach <22876098+anacortesftcrobotics@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
@@ -1,17 +1,22 @@
 package org.firstinspires.ftc.teamcode.chKai;
 
+import com.qualcomm.hardware.rev.RevSPARKMini;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Turret Test")
 public class TurretTest extends LinearOpMode{
     DcMotor turret1;
     DcMotor turret2;
+
+    Servo turnTable;
     Basic basic = new Basic();
     public void runOpMode() {
         turret1 = hardwareMap.get(DcMotor.class, "turret1");
         turret2 = hardwareMap.get(DcMotor.class, "turret2");
+        turnTable = hardwareMap.get(Servo.class, "turnTable");
         basic.initializeHardware(hardwareMap);
         waitForStart();
         if (opModeIsActive()) {
@@ -21,6 +26,8 @@ public class TurretTest extends LinearOpMode{
                 else if (gamepad1.dpad_down){turret1.setPower(-0.25);}
                 else if (gamepad1.dpad_right){turret2.setPower(0.25);}
                 else if (gamepad1.dpad_left){turret2.setPower(-0.25);}
+                else if (gamepad1.right_bumper){turnTable.setPosition(0.25);}
+                else if (gamepad1.left_bumper){turnTable.setPosition(-0.25);}
                 }
             }
         }
