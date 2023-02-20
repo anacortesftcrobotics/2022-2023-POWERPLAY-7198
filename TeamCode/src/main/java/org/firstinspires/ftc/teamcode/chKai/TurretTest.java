83a1b0d3c8ec329bbc235593900cf868049fcab4
KaiG1024 <118638427+KaiG1024@users.noreply.github.com>
2023-02-19 16:53:38 -0800

    TurretTest should be working now, since I initialized the spark mini as a DcMotorSimple.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.rev.RevSPARKMini;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Turret Test")
@@ -11,12 +12,14 @@ public class TurretTest extends LinearOpMode{
     DcMotor turret1;
     DcMotor turret2;
 
-    Servo turnTable;
+    DcMotorSimple turnTable;
     Basic basic = new Basic();
     public void runOpMode() {
         turret1 = hardwareMap.get(DcMotor.class, "turret1");
         turret2 = hardwareMap.get(DcMotor.class, "turret2");
-        turnTable = hardwareMap.get(Servo.class, "turnTable");
+        turnTable = hardwareMap.get(DcMotorSimple.class, "turnTable");
+        turret1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        turret2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         basic.initializeHardware(hardwareMap);
         waitForStart();
         if (opModeIsActive()) {
@@ -26,8 +29,8 @@ public class TurretTest extends LinearOpMode{
                 else if (gamepad1.dpad_down){turret1.setPower(-0.25);}
                 else if (gamepad1.dpad_right){turret2.setPower(0.25);}
                 else if (gamepad1.dpad_left){turret2.setPower(-0.25);}
-                else if (gamepad1.right_bumper){turnTable.setPosition(0.25);}
-                else if (gamepad1.left_bumper){turnTable.setPosition(-0.25);}
+                else if (gamepad1.right_bumper){turnTable.setPower(0.25);}
+                else if (gamepad1.left_bumper){turnTable.setPower(-0.25);}
                 }
             }
         }
