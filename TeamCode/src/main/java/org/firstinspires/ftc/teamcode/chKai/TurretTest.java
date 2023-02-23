1616028e2075cb77ab027346e113227118fbc74b
Kai G <grignkai000@stu.asd103.org>
2023-02-22 16:53:45 -0800

    some improvements in TurretTest
    Signed-off-by: Coach <22876098+anacortesftcrobotics@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/chKai/TurretTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.chKai;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.RevSPARKMini;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navigation.*;
 
 @TeleOp(name = "Turret Test")
 public class TurretTest extends LinearOpMode{
@@ -14,27 +16,68 @@ public class TurretTest extends LinearOpMode{
 
     DcMotorSimple turnTable;
     Basic basic = new Basic();
+
+    BNO055IMU imu, imu2;
+    Orientation angles;
+    Acceleration gravity;
+    BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
     public void runOpMode() {
         turret1 = hardwareMap.get(DcMotor.class, "turret1");
         turret2 = hardwareMap.get(DcMotor.class, "turret2");
         turnTable = hardwareMap.get(DcMotorSimple.class, "turnTable");
         turret1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         turret2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        turret2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        turret1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        turret2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         basic.initializeHardware(hardwareMap);
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu2 = hardwareMap.get(BNO055IMU.class, "imu2");
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        imu.initialize(parameters);
+        imu2.initialize(parameters);
+
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 basic.go(gamepad1);
-                if (gamepad1.dpad_up){turret1.setPower(0.25);}
-                else if (gamepad1.dpad_down){turret1.setPower(-0.25);}
-                else if (gamepad1.dpad_right){turret2.setPower(0.25);}
-                else if (gamepad1.dpad_left){turret2.setPower(-0.25);}
+                if (gamepad1.dpad_up){turret1.setPower(1.0);}
+                else if (gamepad1.dpad_down){turret1.setPower(-0.3);}
+                else if (gamepad1.dpad_right) {
+                    turret2.setPower(1);
+                    /*turret2.setTargetPosition(10);
+                    turret2.setPower(0.5);
+                    turret2.setMode(DcMotor.RunMode.RUN_TO_POSITION);*/
+                }
+                else if (gamepad1.dpad_left){
+                    turret2.setPower(-0.3);
+                    /*turret2.setTargetPosition(2);
+                    turret2.setPower(0.3);
+                    turret2.setMode(DcMotor.RunMode.RUN_TO_POSITION);*/
+                }
                 else if (gamepad1.right_bumper){turnTable.setPower(0.25);}
                 else if (gamepad1.left_bumper){turnTable.setPower(-0.25);}
+                else {
+                    turret1.setPower(0);
+                    turret2.setPower(0);
+                    turnTable.setPower(0);
                 }
+                telemetry.addData("turret2 position", turret2.getCurrentPosition());
+                telemetry.addData("worm position", turret1.getCurrentPosition());
+                telemetry.addData("imu", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+                telemetry.addData("imu2", imu2.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+                telemetry.update();
             }
         }
     }
+}
 
 
 
